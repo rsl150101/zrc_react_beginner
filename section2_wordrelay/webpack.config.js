291479a6a0643655e4532a5803faab81ddd7ada5
rsl150101 <dnwnd501@naver.com>
2024-10-06 19:20:34 +0900

    [Initialize] 2-4 Configure webpack and modularize files

--- section2_wordrelay/webpack.config.js
+++ section2_wordrelay/webpack.config.js
@@ -1,1 +1,12 @@
-module.expots = {};
+const path = require("path");
+
+module.expots = {
+  mode: "development",
+  entry: {
+    app: ["./client.jsx"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  },
+};
