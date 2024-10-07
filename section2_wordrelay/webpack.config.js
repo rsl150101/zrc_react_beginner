53c6021a4ab3a57db3bd8320cb3ba218bd65181b
rsl150101 <dnwnd501@naver.com>
2024-10-07 16:04:44 +0900

    [Add] babel-loader exclude node_modules in webpack.config.js

--- section2_wordrelay/webpack.config.js
+++ section2_wordrelay/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        exclude: /node_modules/,
         loader: "babel-loader",
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
