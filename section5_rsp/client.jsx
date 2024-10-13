7976e0b9efa85ac8dcf12c6c0fce885e8c356059
rsl150101 <dnwnd501@naver.com>
2024-10-13 15:36:09 +0900

    [Initialize] RSP App setttings

---
+++ section5_rsp/client.jsx
@@ -0,0 +1,6 @@
+const React = require("react");
+const { createRoot } = require("react-dom/client");
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<React.StrictMode></React.StrictMode>);
