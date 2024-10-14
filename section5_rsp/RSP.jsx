1c5dc86f26a34a2bbac4b1531b46a048d3f5e85f
rsl150101 <dnwnd501@naver.com>
2024-10-14 15:24:30 +0900

    [Use] 5-5 function component that uses react hooks instead of class component in RSP App

--- section5_rsp/RSP.jsx
+++ section5_rsp/RSP.jsx
@@ -1,5 +1,5 @@
 const React = require("react");
-const { Component } = React;
+const { useState, useRef, useEffect } = React;
 
 const rspCoords = {
   scissors: "-140px",
@@ -7,7 +7,7 @@ const rspCoords = {
   paper: "-287px",
 };
 
-const score = {
+const scores = {
   scissors: -1,
   rock: 0,
   paper: 1,
@@ -17,95 +17,79 @@ const getComputerRsp = (imgCoord) => {
   return Object.keys(rspCoords).find((key) => rspCoords[key] === imgCoord);
 };
 
-class RSP extends Component {
-  state = { result: "", imgCoord: "0", score: 0, shoot: false };
-  interval;
+const RSP = () => {
+  const [result, setResult] = useState("");
+  const [imgCoord, setImgCoord] = useState(rspCoords.rock);
+  const [score, setScore] = useState(0);
+  const [shoot, setShoot] = useState(false);
+  const intervalRef = useRef(null);
 
   changeHand = () => {
-    const { imgCoord } = this.state;
     if (imgCoord === rspCoords.scissors) {
-      this.setState({ imgCoord: rspCoords.paper });
+      setImgCoord(rspCoords.paper);
     } else if (imgCoord === rspCoords.rock) {
-      this.setState({ imgCoord: rspCoords.scissors });
+      setImgCoord(rspCoords.scissors);
     } else if (imgCoord === rspCoords.paper) {
-      this.setState({ imgCoord: rspCoords.rock });
+      setImgCoord(rspCoords.rock);
     }
   };
 
-  componentDidMount() {
-    this.interval = setInterval(this.changeHand, 100);
-  }
+  useEffect(() => {
+    intervalRef.current = setInterval(changeHand, 100);
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, [imgCoord]);
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  handleRspClickBtn = (chosen) => {
-    this.setState((prevState) => ({ shoot: !prevState.shoot }));
-    clearInterval(this.interval);
-    const userChosen = score[chosen];
-    const { imgCoord } = this.state;
-    const computerChosen = score[getComputerRsp(imgCoord)];
+  handleRspClickBtn = (chosen) => () => {
+    setShoot((prevShoot) => !prevShoot);
+    clearInterval(intervalRef.current);
+    const userChosen = scores[chosen];
+    const computerChosen = scores[getComputerRsp(imgCoord)];
     const diff = userChosen - computerChosen;
 
     if (diff === 0) {
-      this.setState({ result: "Draw" });
+      setResult("Draw");
     } else if ([-1, 2].includes(diff)) {
-      this.setState((prevState) => ({
-        result: "Lose",
-        score: prevState.score - 1,
-      }));
+      setResult("Lose");
+      setScore((prevScore) => prevScore - 1);
     } else {
-      this.setState((prevState) => ({
-        result: "Win",
-        score: prevState.score + 1,
-      }));
+      setResult("Win");
+      setScore((prevScore) => prevScore + 1);
     }
     setTimeout(() => {
-      this.setState((prevState) => ({ shoot: !prevState.shoot }));
-      this.interval = setInterval(this.changeHand, 100);
+      setShoot((prevShoot) => !prevShoot);
+      intervalRef.current = setInterval(changeHand, 100);
     }, 2000);
   };
 
-  render() {
-    const { imgCoord, result, score, shoot } = this.state;
-    return (
-      <>
-        <div
-          className="computer"
-          style={{
-            background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
-          }}
-        />
-        <div>
-          <button
-            onClick={() => this.handleRspClickBtn("scissors")}
-            disabled={shoot}
-          >
-            Scissors
-          </button>
-          <button
-            onClick={() => this.handleRspClickBtn("rock")}
-            disabled={shoot}
-          >
-            Rock
-          </button>
-          <button
-            onClick={() => this.handleRspClickBtn("paper")}
-            disabled={shoot}
-          >
-            Paper
-          </button>
-        </div>
-        <div>
-          <h2>{result}</h2>
-        </div>
-        <div>
-          <h4>score : {score}</h4>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div
+        className="computer"
+        style={{
+          background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
+        }}
+      />
+      <div>
+        <button onClick={handleRspClickBtn("scissors")} disabled={shoot}>
+          Scissors
+        </button>
+        <button onClick={handleRspClickBtn("rock")} disabled={shoot}>
+          Rock
+        </button>
+        <button onClick={handleRspClickBtn("paper")} disabled={shoot}>
+          Paper
+        </button>
+      </div>
+      <div>
+        <h2>{result}</h2>
+      </div>
+      <div>
+        <h4>score : {score}</h4>
+      </div>
+    </>
+  );
+};
 
 module.exports = RSP;
