9d33c269375e13680d1a51d16e7b87ac6d1656a7
rsl150101 <dnwnd501@naver.com>
2024-10-20 16:06:20 +0900

    [Add] 6-2 Lotto number every second render using setTimeout and redo button

--- section6_lotto/Lotto.jsx
+++ section6_lotto/Lotto.jsx
@@ -10,26 +10,64 @@ const getWinNumbers = () => {
       candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
     );
   }
-  return chosen;
+  const winNumbers = chosen.slice(0, 6).sort((p, c) => p - c);
+  const bonusNumber = chosen[chosen.length - 1];
+  return [...winNumbers, bonusNumber];
 };
 
 class Lotto extends Component {
   state = {
     winNumbers: getWinNumbers(),
+    winBalls: [],
+    bonus: null,
+    redo: false,
   };
-  render() {
+
+  timeouts = [];
+
+  componentDidMount() {
     const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => ({
+          winBalls: [...prevState.winBalls, winNumbers[i]],
+        }));
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[winNumbers.length - 1] = setTimeout(() => {
+      this.setState({ bonus: winNumbers[winNumbers.length - 1], redo: true });
+    }, winNumbers.length * 1000);
+  }
+  componentWillUnmount() {
+    this.timeouts.forEach((t) => {
+      clearTimeout(t);
+    });
+  }
+
+  handleRedoBtn = () => {
+    this.setState({
+      winNumbers: getWinNumbers(),
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    });
+    this.timeouts = [];
+  };
+
+  render() {
+    const { winBalls, redo, bonus } = this.state;
 
     return (
       <>
         <h1>Lotto Number</h1>
         <div>
-          {winNumbers.map((v, i) => {
-            if (i < 6) return <big key={i}> {v} </big>;
-          })}
+          {winBalls.map((v) => (
+            <big key={`win_number_${v}`}> {v} </big>
+          ))}
         </div>
         <h1>Bonus Number</h1>
-        <big>{winNumbers[6]}</big>
+        <big>{bonus}</big>
+        <div>{redo && <button onClick={this.handleRedoBtn}>Redo</button>}</div>
       </>
     );
   }
