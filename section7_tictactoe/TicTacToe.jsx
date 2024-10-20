8054bb13e7e661e236f505b9f6b077129a5b34d9
rsl150101 <dnwnd501@naver.com>
2024-10-20 20:19:53 +0900

    [Update] 7-1 Initialize state with useReducer

--- section7_tictactoe/TicTacToe.jsx
+++ section7_tictactoe/TicTacToe.jsx
@@ -1,7 +1,20 @@
 import React, { useReducer } from "react";
 import Table from "./Table";
 
+const initialState = {
+  winner: "",
+  turn: 0,
+  tableData: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+};
+
+const reducer = (state, action) => {};
+
 const TicTacToe = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return;
 };
 
