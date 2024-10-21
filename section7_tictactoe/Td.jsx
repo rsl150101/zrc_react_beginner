48f3257ef029e09d0b1d428f85eb111e3356bad1
rsl150101 <dnwnd501@naver.com>
2024-10-21 20:00:08 +0900

    [Add] 7-4 win, loss, draw logic

--- section7_tictactoe/Td.jsx
+++ section7_tictactoe/Td.jsx
@@ -1,11 +1,13 @@
 import React, { useCallback } from "react";
-import { CHANGE_TURN, CLICK_CELL } from "./TicTacToe";
+import { CLICK_CELL } from "./TicTacToe";
 
 const Td = ({ cellData, rowIndex, cellIndex, dispatch }) => {
   const handleTdClick = useCallback(() => {
+    if (cellData) {
+      return;
+    }
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
-    dispatch({ type: CHANGE_TURN });
-  });
+  }, [cellData]);
   return <td onClick={handleTdClick}>{cellData}</td>;
 };
 
--- section7_tictactoe/TicTacToe.jsx
+++ section7_tictactoe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useCallback, useEffect, useReducer } from "react";
 import Table from "./Table";
 
 const initialState = {
@@ -9,10 +9,13 @@ const initialState = {
     ["", "", ""],
     ["", "", ""],
   ],
+  recentCell: [-1, -1],
 };
 
 export const CLICK_CELL = "CLICK_CELL";
 export const CHANGE_TURN = "CHANGE_TURN";
+const SET_WINNER = "SET_WINNER";
+const RESET_GAME = "RESET_GAME";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -23,6 +26,7 @@ const reducer = (state, action) => {
       return {
         ...state,
         tableData,
+        recentCell: [action.row, action.cell],
       };
     }
     case CHANGE_TURN: {
@@ -31,16 +35,99 @@ const reducer = (state, action) => {
         turn: state.turn === "O" ? "X" : "O",
       };
     }
+
+    case SET_WINNER: {
+      return {
+        ...state,
+        winner: action.winner,
+      };
+    }
+    case RESET_GAME: {
+      return {
+        ...state,
+        turn: "O",
+        tableData: [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""],
+        ],
+        recentCell: [-1, -1],
+      };
+    }
   }
 };
 
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { tableData } = state;
+  const { tableData, recentCell, turn, winner } = state;
+  useEffect(() => {
+    const [row, cell] = recentCell;
+    if (row < 0) {
+      return;
+    }
+    let win = false;
+    if (
+      tableData[row][0] === turn &&
+      tableData[row][1] === turn &&
+      tableData[row][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][cell] === turn &&
+      tableData[1][cell] === turn &&
+      tableData[2][cell] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][0] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][2] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][0]
+    ) {
+      win = true;
+    }
+
+    if (win) {
+      dispatch({ type: SET_WINNER, winner: turn });
+      dispatch({ type: RESET_GAME });
+    } else {
+      let all = true;
+      tableData.forEach((row) => {
+        row.forEach((cell) => {
+          if (!cell) {
+            all = false;
+          }
+        });
+      });
+      if (all) {
+        dispatch({ type: SET_WINNER, winner: "draw" });
+        dispatch({ type: RESET_GAME });
+      } else {
+        dispatch({ type: CHANGE_TURN });
+      }
+    }
+  }, [recentCell]);
+
+  const onClickTable = useCallback(() => {
+    dispatch({ type: SET_WINNER, winner: "O" });
+  }, []);
 
   return (
     <>
-      <Table tableData={tableData} dispatch={dispatch} />
+      <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch} />
+      {winner === "draw" ? (
+        <h1>Draw</h1>
+      ) : winner === "O" || winner === "X" ? (
+        <h1>{`Winner : ${winner}`}</h1>
+      ) : null}
     </>
   );
 };
